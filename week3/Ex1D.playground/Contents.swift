a038511cf85926514c78b3cf790134749d010a94
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-11-20 09:53:17 +0800

    Ex1D done

--- week3/Ex1D.playground/Contents.swift
+++ week3/Ex1D.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let age = 2
+
+if age < 5 {
+    print("Free Entry")
+}
+else if age >= 5 && age <= 12{
+    print("Ticket price is 10$")
+}
+else if age >= 13 && age <= 64{
+    print("Ticket price is 15$")
+}
+else{
+    print("Ticket price is 8$")
+}
