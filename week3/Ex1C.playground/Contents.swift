4fa395ee4e32076f2cedbe8acb25989bc0f4a40b
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-11-20 09:34:48 +0800

    Ex1C done

--- week3/Ex1C.playground/Contents.swift
+++ week3/Ex1C.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let leapYear = 2024
+
+if leapYear % 4 == 0 && (leapYear % 100 != 0 || leapYear % 400 == 0){
+    print("Leap Year")
+}
+else
+{
+    print("Not a Leap Year")
+}
