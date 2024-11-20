cf5a93b2b6373f36c0050a957dd4daa969b8bf2d
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-11-20 09:48:52 +0800

    Ex1E done

---
+++ week3/Ex1E.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let username:String = "username"
+let password:String = "123"
+
+let inputUsername:String = "usernamee"
+let inputPassword:String = "123"
+
+if username == inputUsername && password == inputPassword{
+    print("Login successfully")
+}
+else if username == inputUsername && password == inputPassword{
+    print("Invalid Password")
+}
+else {
+    print("Invalid Username")
+}
