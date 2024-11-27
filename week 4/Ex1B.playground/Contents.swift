77154bfebd011b2c2e158f819198c625187a8703
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-11-27 09:46:19 +0800

    Ex1B done

--- week 4/Ex1B.playground/Contents.swift
+++ week 4/Ex1B.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var number = 2534
+var strnumber = String(number)
+var result: Int = 0
+
+for char in strnumber {
+    if let int = Int(String(char)) {
+        result += int
+    }
+}
+print(result)
