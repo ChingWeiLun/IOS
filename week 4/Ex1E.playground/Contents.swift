51ed2e692591dc9f311492fe3629142a67fe524b
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-11-27 10:20:17 +0800

    Ex1E done

--- week 4/Ex1E.playground/Contents.swift
+++ week 4/Ex1E.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let number = [3,5,1,4]
+
+for i in number {
+    for j in 1...i {
+        print("*", terminator: "")
+    }
+    print(terminator: "\n")
+}
