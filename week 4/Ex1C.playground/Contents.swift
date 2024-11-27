c2ce045566691a893b49be01204c45632da25539
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-11-27 10:07:17 +0800

    Ex1C done

--- week 4/Ex1C.playground/Contents.swift
+++ week 4/Ex1C.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+var numberArray = [43,3,5,7,8]
+var smallest = numberArray[0]
+var largest = numberArray[0]
+
+
+for number in numberArray {
+    if number < smallest {
+        smallest = number
+    }
+    else if number > largest {
+        largest = number
+    }
+}
+print("Smallest number is \(smallest)")
+print("Largest number is \(largest)")
