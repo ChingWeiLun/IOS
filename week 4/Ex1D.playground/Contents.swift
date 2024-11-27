e9404c3abccdbd8c255054ef4eae30626e4778b5
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-11-27 10:11:12 +0800

    Ex1D done

--- week 4/Ex1D.playground/Contents.swift
+++ week 4/Ex1D.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+for number in 1...20{
+    if number % 3 == 0{
+        print("\(number) = Fizz")
+    }
+    if number % 5 == 0{
+        print("\(number) = Buzz")
+    }
+    if number % 3 == 0 && number % 5 == 0{
+        print("\(number) = FizzBuzz")
+    }
+}
