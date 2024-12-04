b9359ebe7ccefb532eec96908225e247cf7480fd
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 09:37:33 +0800

    Ex1B function done

--- week 4/Ex1B.playground/Contents.swift
+++ week 4/Ex1B.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-var number = 2534
+/*var number = 2534
 var strnumber = String(number)
 var result: Int = 0
 
@@ -9,4 +9,18 @@ for char in strnumber {
         result += int
     }
 }
-print(result)
+print(result)*/
+
+func sumOfDigits(number:Int)->Int{
+    var strnumber = String(number)
+    var result: Int = 0
+
+    for char in strnumber {
+        if let int = Int(String(char)) {
+            result += int
+        }
+    }
+    return result
+}
+
+print(sumOfDigits(number: 2534))
