3b0eebafb913ca9c5331b565a2c244994dc284ab
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 10:23:08 +0800

    Ex1E function done

--- week 4/Ex1E.playground/Contents.swift
+++ week 4/Ex1E.playground/Contents.swift
@@ -1,10 +1,20 @@
 import UIKit
 
-let number = [3,5,1,4]
+/*let number = [3,5,1,4]
 
 for i in number {
     for j in 1...i {
         print("*", terminator: "")
     }
     print(terminator: "\n")
+}*/
+
+func histogramGenerator(number: [Int]) {
+    for i in number {
+        for j in 1...i {
+            print("*", terminator: "")
+        }
+        print(terminator: "\n")
+    }
 }
+print(histogramGenerator(number: [3,5,1,4]))
