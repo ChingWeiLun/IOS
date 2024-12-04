aa9dd0387fb3e87cae377ed142206baa4827e959
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 10:39:33 +0800

    Ex2B done

--- week 4/Ex2B.playground/Contents.swift
+++ week 4/Ex2B.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func checkEvenOdd(start:Int,end:Int)->(even:Int,odd:Int){
+    var even = 0
+    var odd = 0
+    for i in start...end{
+        if i % 2 == 0{
+            even += 1
+        }
+        else{
+            odd += 1
+        }
+    }
+    return (even,odd)
+}
+
+let result = checkEvenOdd(start:1,end:15)
+print("Even numbers: \(result.even)")
+print("Odd numbers: \(result.odd)")
