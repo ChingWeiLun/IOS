8ee912705212204479bc21467027368e348fb330
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 10:44:39 +0800

    Ex2C done

--- week 4/Ex2C.playground/Contents.swift
+++ week 4/Ex2C.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func multiplyNumber(n:Int){
+    if n > 0{
+        for i in 1...10{
+            print("\(n) x \(i) = \(n * i)")
+        }
+    }
+    else{
+        print("Please enter a positive number.")
+    }
+}
+
+multiplyNumber(n: 4)
