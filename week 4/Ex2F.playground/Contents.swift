b2f8d97b9954e5bce6507b1592746f601d73c705
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 11:20:26 +0800

    Ex2F done

--- week 4/Ex2F.playground/Contents.swift
+++ week 4/Ex2F.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func calculateRectangle(width:Int,height:Int)->(area:Int,perimeter:Int){
+    var area:Int = width * height
+    var perimeter:Int = 2 * (width + height)
+    return (area,perimeter)
+}
+
+var result = calculateRectangle(width: 4, height: 7)
+print("Area: \(result.area), Perimeter: \(result.perimeter)")
