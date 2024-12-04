ab188625a1b0b3fe5a9ede76ce166f11804d83d0
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 10:27:52 +0800

    Ex1G function done

--- week 4/Ex1F.playground/Contents.swift
+++ week 4/Ex1F.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-var num = 4
+/*var num = 4
 
 for i in 1...num {
     for j in 1...i {
@@ -17,7 +17,26 @@ for y in 1...num1 {
     }
     print("")
     num1-=1
-}
+}*/
 
+func halfDiamondPattern(maxstar:Int){
+    for i in 1...maxstar {
+        for j in 1...i {
+            print("*", terminator: "")
+        }
+        print("")
+    }
+
+    var num1 = maxstar-1
+
+    for y in 1...num1 {
+        for x in 1...num1 {
+            print("*", terminator: "")
+        }
+        print("")
+        num1-=1
+    }
+}
+halfDiamondPattern(maxstar: 4)
     
 
