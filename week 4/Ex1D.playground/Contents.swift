dc39c8dcb2d2e768deb3b85afc435f12eff7889a
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 10:15:12 +0800

    Ex1D function done

--- week 4/Ex1D.playground/Contents.swift
+++ week 4/Ex1D.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-for number in 1...20{
+/*for number in 1...20{
     if number % 3 == 0{
         print("\(number) = Fizz")
     }
@@ -10,4 +10,21 @@ for number in 1...20{
     if number % 3 == 0 && number % 5 == 0{
         print("\(number) = FizzBuzz")
     }
+}*/
+
+func fizzBuzz(start:Int,end:Int)->String{
+    var sentences:String = ""
+    for number in start...end{
+        if number % 3 == 0 && number % 5 == 0{
+            sentences = sentences + "\(number) = FizzBuzz\n"
+        }
+        else if number % 5 == 0{
+            sentences = sentences + "\(number) = Buzz\n"
+        }
+        else if number % 3 == 0{
+            sentences = sentences + "\(number) = Fizz\n"
+        }
+    }
+    return sentences
 }
+print(fizzBuzz(start:1,end:20))
