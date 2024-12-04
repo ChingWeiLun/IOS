ba0ac641fb7cf4fb67d1d2a3b9c65f579c7adfaa
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 09:52:56 +0800

    Ex1C function done

--- week 4/Ex1C.playground/Contents.swift
+++ week 4/Ex1C.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 
-var numberArray = [43,3,5,7,8]
+/*var numberArray = [43,3,5,7,8]
 var smallest = numberArray[0]
 var largest = numberArray[0]
 
@@ -15,4 +15,23 @@ for number in numberArray {
     }
 }
 print("Smallest number is \(smallest)")
-print("Largest number is \(largest)")
+print("Largest number is \(largest)")*/
+
+func findMinAndMax(numbers:[Int])->(smallest:Int,largest:Int){
+    var smallest = numbers[0]
+    var largest = numbers[0]
+    
+    for number in numbers {
+        if number < smallest {
+            smallest = number
+        }
+        else if number > largest {
+            largest = number
+        }
+    }
+    return(smallest, largest)
+}
+
+let result = findMinAndMax(numbers: [43,3,5,7,8])
+print("Smallest number is \(result.smallest)")
+print("Largest number is \(result.largest)")
