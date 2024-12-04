fd8d303468eef955ceeebe41bffb02f10f968da2
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 11:12:52 +0800

    Ex2D done

--- week 4/Ex2D.playground/Contents.swift
+++ week 4/Ex2D.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func factorial(n:Int)->Int{
+    var result = 1
+    for i in 1...n{
+        result *= i
+    }
+    return result
+}
+
+print(factorial(n: 5))
