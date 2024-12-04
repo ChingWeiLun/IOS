a307c396ad4af556b854653e5d4c5a085b69466c
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-04 11:16:44 +0800

    Ex2E done

--- week 4/Ex2E.playground/Contents.swift
+++ week 4/Ex2E.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func sumInRange(start:Int,end:Int)->Int{
+    var sum:Int=0
+    for i in start...end{
+        sum+=i
+    }
+    return sum
+}
+
+print(sumInRange(start:3,end:6))
