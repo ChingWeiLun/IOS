de954d17a7e63eb7d05a55f0fca863a8f32633f3
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2024-12-18 11:24:31 +0800

    week 7 done

--- week 7/FavouriteArtist/FavouriteArtist/ContentView.swift
+++ week 7/FavouriteArtist/FavouriteArtist/ContentView.swift
@@ -9,12 +9,107 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                Spacer()
+                Image(systemName: "bell.badge")
+                Image(systemName: "person")
+                    .padding(.trailing, 10.0)
+            }
         }
+        HStack{
+            Text("Favuorite Artist")
+                .font(.title)
+                .bold()
+                .padding(.leading)
+            Spacer()
+        }
+        VStack{
+            Color("gray")
+                .frame(height: 150)
+                .cornerRadius(15)
+                .overlay(
+                        HStack{
+                            Text("Milet")
+                                .font(.system(size: 24))
+                                .foregroundColor(Color("darkmode"))
+                                .multilineTextAlignment(.leading)
+                                .padding(.leading, 25.0)
+                                .padding(.top, 100.0)
+                            Spacer()
+                            Image("milet")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 130, height: 130)
+                                .padding(.trailing, 30.0)
+                                .padding(.top, 18.0)
+                        }
+                )
+            Color("blue")
+                .frame(height: 150)
+                .cornerRadius(15)
+                .overlay(
+                        HStack{
+                            Text("Zarame")
+                                .font(.system(size: 24))
+                                .foregroundColor(Color("darkmode"))
+                                .multilineTextAlignment(.leading)
+                                .padding(.leading, 25.0)
+                                .padding(.top, 100.0)
+                            Spacer()
+                            Image("zarame")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 130, height: 130)
+                                .padding(.trailing, 30.0)
+                                .padding(.top, 18.0)
+                            
+                        }
+                )
+            Color("red")
+                .frame(height: 150)
+                .cornerRadius(15)
+                .overlay(
+                        HStack{
+                            Text("Joji")
+                                .font(.system(size: 24))
+                                .foregroundColor(Color("darkmode"))
+                                .multilineTextAlignment(.leading)
+                                .padding(.leading, 25.0)
+                                .padding(.top, 100.0)
+                                
+                            Spacer()
+                            Image("joji")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 130, height: 130)
+                                .padding(.trailing, 30.0)
+                                .padding(.top, 18.0)
+                        }
+                )
+            Color("lightblue")
+                .frame(height: 150)
+                .cornerRadius(15)
+                .overlay(
+                        HStack{
+                            Text("Satomi Ishihara")
+                                .font(.system(size: 24))
+                                .foregroundColor(Color("darkmode"))
+                                .multilineTextAlignment(.leading)
+                                .padding(.leading, 25.0)
+                                .padding(.top, 100.0)
+                                
+                            Spacer()
+                            Image("satomi")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 130, height: 130)
+                                .padding(.trailing, 30.0)
+                                .padding(.top, 18.0)
+                        }
+                )
+        }
+        
         .padding()
     }
 }
