bc6a41c555a2e1e937b4fa03bb6b8f6cd6accd53
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2025-01-08 11:55:35 +0800

    week8 (weather done)

---
+++ week 8/Weather Data/Weather Data/Weather_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_DataApp.swift
+//  Weather Data
+//
+//  Created by labuser on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
