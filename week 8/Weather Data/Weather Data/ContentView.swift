8d758e09cfe3c15dc10d63eabbc91fae6a50c6a8
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2025-01-08 11:55:38 +0800

    week8 (weather done)

--- week 8/Weather Data/Weather Data/ContentView.swift
+++ week 8/Weather Data/Weather Data/ContentView.swift
@@ -9,13 +9,120 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack{
+            LinearGradient(gradient: Gradient(
+                colors: [.blue, .white]),
+                           startPoint: .top,
+                           endPoint: .bottom
+            )
+            VStack{
+                Text("Selangor, Malaysia")
+                    .font(.system(size: 30))
+                    .bold()
+                    .foregroundColor(Color.white)
+                    .padding(.top, 70)
+                Image(systemName: "cloud.sun.fill")
+                    .resizable()
+                    .foregroundStyle(.white, .yellow)
+                    .frame(width: 180, height: 160)
+                    .padding(.top, 15)
+                Text("30°C")
+                    .font(.system(size: 65))
+                    .bold()
+                    .foregroundColor(Color.white)
+                
+                
+            HStack{
+                VStack{
+                    Text("TUE")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    Image(systemName: "sun.rain.fill")
+                        .resizable()
+                        .foregroundStyle(.blue, .yellow)
+                        .frame(width: 45, height: 45)
+                    Text("17°C")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                }.padding()
+                VStack{
+                    Text("WED")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    Image(systemName: "sun.rain.fill")
+                        .resizable()
+                        .foregroundStyle(.blue, .yellow)
+                        .frame(width: 45, height: 45)
+                    Text("17°C")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    }.padding()
+                VStack{
+                    Text("THU")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    Image(systemName: "sun.rain.fill")
+                        .resizable()
+                        .foregroundStyle(.blue, .yellow)
+                        .frame(width: 45, height: 45)
+                    Text("17°C")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    }.padding()
+                VStack{
+                    Text("FRI")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    Image(systemName: "sun.rain.fill")
+                        .resizable()
+                        .foregroundStyle(.blue, .yellow)
+                        .frame(width: 45, height: 45)
+                    Text("17°C")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    }.padding()
+                VStack{
+                    Text("SAT")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    Image(systemName: "sun.rain.fill")
+                        .resizable()
+                        .foregroundStyle(.blue, .yellow)
+                        .frame(width: 45, height: 45)
+                    Text("17°C")
+                        .font(.system(size: 20))
+                        .foregroundStyle(Color.white)
+                        .padding(.top, 5)
+                    }.padding()
+                }
+                Spacer()
+                HStack{
+                    ZStack{
+                        Color.white
+                            .frame(width: 250, height: 50)
+                            .cornerRadius(15)
+                            .padding(.bottom, 150)
+                            
+                        Text("Change Day time")
+                            .foregroundColor(Color.blue)
+                            .padding(.bottom, 150)
+                        
+                    }
+                    
+                }
+            }
+            
+            
+        }.ignoresSafeArea()
     }
 }
 
