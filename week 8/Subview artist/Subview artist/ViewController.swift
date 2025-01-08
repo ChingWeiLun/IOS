2ae05fd7de4ae48a5302a398fff45d4526cead03
ChingWeiLun <chingweilun0815@e.neivce.edu.my>
2025-01-08 10:53:08 +0800

    week8 (subview done)

---
+++ week 8/Subview artist/Subview artist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subview artist
+//
+//  Created by labuser on 08/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
